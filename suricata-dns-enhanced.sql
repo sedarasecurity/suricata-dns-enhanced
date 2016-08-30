4de18b34c4655be01a382f8b18cecd443e4fff07
cabrel <robin.harper@sedarasecurity.com>
2016-08-30 13:33:25 -0500

    added rule block for the response session log message

--- suricata-dns-enhanced.sql
+++ suricata-dns-enhanced.sql
@@ -1,9 +1,10 @@
 DELETE FROM plugin WHERE id = "88876";
 DELETE FROM plugin_sid where plugin_id = "88876";
 INSERT INTO plugin (id, type, name, description, product_type, vendor) VALUES (88876, 1, 'suricata-dns-enhanced', 'Suricata DNS', 4, 'Open Information Security Foundation');
-INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 1, 15, 18, 2, 2, 'DNS Query for Internal Resource');
-INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 2, 15, 18, 2, 2, 'DNS Query for External Resource');
-INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 3, 15, 18, 2, 2, 'Unknown DNS Query');
-INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 4, 15, 18, 2, 2, 'External DNS Query for Internal Resource');
-INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 5, 15, 18, 2, 2, 'Internal DNS Query for External Resource');
-INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 6, 15, 18, 2, 2, 'FQDN DNS Query');
+INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 1, 15, 18, 2, 2, 'Suricata DNS: Query for Internal Resource');
+INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 2, 15, 18, 2, 2, 'Suricata DNS: Query for External Resource');
+INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 3, 15, 18, 2, 2, 'Suricata DNS: Unknown Query');
+INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 4, 15, 18, 2, 2, 'Suricata DNS: External Query for Internal Resource');
+INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 5, 15, 18, 2, 2, 'Suricata DNS: Internal Query for External Resource');
+INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 6, 15, 18, 2, 2, 'Suricata DNS: FQDN Query');
+INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`category_id`,`subcategory_id`,`reliability`,`priority`,`name`) VALUES (88876, 100, 8, 57, 2, 2, 'Suricata DNS: Response Session');
